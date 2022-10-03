14cf06d26a45ea80dc88495a5896c2145a118427
Elena <kallaur@bk.ru>
2022-10-03 16:08:00 -0400

    done

---
+++ drill5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int y = - n;
+while (y<= n | n<= -y)
+
+{Console.Write(y);
+Console.Write(" ");
+y++;
+}
+Console.WriteLine(" ");
