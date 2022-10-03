5c37305b8f5a12d61410aac02e342ad98e3aa3fc
Elena <kallaur@bk.ru>
2022-10-03 17:26:20 -0400

    done

---
+++ drill2_home/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Не стесняйтесь - введите еще одно");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вам, наверно, интрересно какое из введенных вами чисел больше? Отвечаем ");
+
+if (a>b)
+
+{Console.WriteLine("Первое !");
+ Console.WriteLine(a);
+}
+
+else
+
+ if (a==b)
+    {Console.WriteLine("Введенные вами числа равны");
+    }
+    
+else 
+{Console.WriteLine("Второе!");
+Console.WriteLine(b);}
