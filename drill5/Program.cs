35f0cd031e14a624a8942f29243cebe5445e4d46
Elena <kallaur@bk.ru>
2022-10-03 16:46:57 -0400

    donedone

--- drill5/Program.cs
+++ drill5/Program.cs
@@ -1,10 +1,15 @@
-﻿Console.WriteLine("Введите целое положительное число");
+﻿Console.WriteLine("Введите целое положительное число (желательно не больше 20;)");
 int n = Convert.ToInt32(Console.ReadLine());
-int y = - n;
+if (n>20)
+Console.WriteLine("Не хорошо получилось. Adios!");
+
+else 
+   { int y = - n;
 while (y<= n)
 
-{Console.Write(y);
-Console.Write(" ");
+{Console.Write(y); Console.Write(" ");
+
 y++;
 }
-Console.WriteLine(" ");
+Console.WriteLine(" ");}
+
