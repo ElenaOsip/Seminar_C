7e990ab1cd931c8c273f47cf4464fd5a9a583b0f
Elena <kallaur@bk.ru>
2022-10-03 16:36:40 -0400

    done

---
+++ drill7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое трехзначное положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (100<=n && n<=999)
+{int LastDigit = n % 10;
+Console.Write("Последняя цифра данного числа = ");
+Console.WriteLine (LastDigit);
+}
+else 
+{Console.WriteLine("Будь внимательней при чтении условий задания. До свидания.");}
+
