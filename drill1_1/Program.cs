23c04dd8959aaf049be34399ca112cb06a741a7c
Elena <kallaur@bk.ru>
2022-10-03 02:16:54 -0400

    super

---
+++ drill1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+while (count<4)
+
+    {   Console.WriteLine("Введите первое число");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int y = Convert.ToInt32(Console.ReadLine());
+    if (n == y*y)
+       { Console.WriteLine("Да! Первое число это квадрат второго)");}
+
+    else 
+    {Console.WriteLine("Нет, это не квадрат первого числа");}
+
+    count++;
+    }        
+    Console.WriteLine("Отлично! На сегодня достаточно)");
