f7568480ccb871a44c902ea4fb95eaa335bf72a0
Elena <kallaur@bk.ru>
2022-10-04 14:06:33 -0400

    remainer of div

---
+++ drill13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое целое число");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first%second == 0)
+
+ {Console.WriteLine("Отлично! Делится без остатка!!");}
+
+else 
+{Console.WriteLine("Остаток от деления = " + first%second);}
