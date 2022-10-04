fe14a0a9bdaf4d6d61868760f95781a20abdef02
Elena <kallaur@bk.ru>
2022-10-04 14:54:55 -0400

    kvadr

---
+++ drill16_kvadrats/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b || b == a*a)
+
+{Console.WriteLine("Одно из чисел является квадратом другого");}
+
+else
+{Console.WriteLine("Ни одно из введенных чисел не явялется квадратом другого");
+    
+}
