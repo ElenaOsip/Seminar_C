a35993e974fa4622ef28cb652339bb5a26ea18b3
Elena <kallaur@bk.ru>
2022-10-04 14:35:35 -0400

    div

---
+++ drill14_7_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое целое число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%7 == 0 && n%23 == 0)
+
+    {Console.WriteLine("Введенное вами число делится и на 7 и на 23");}
+
+else 
+    { Console.WriteLine("Увы и ах, не делится на 7 и/или 23");}
