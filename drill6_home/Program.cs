43d45d3bcdd6a66db6834f8fedd6e123ac2764b8
Elena <kallaur@bk.ru>
2022-10-03 20:44:31 -0400

    done

---
+++ drill6_home/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите любое целое число");
+int even = Convert.ToInt32(Console.ReadLine());
+
+if (even%2 == 0)
+
+    Console.WriteLine("Ввведенное вами число является четным");
+else
+
+    Console.WriteLine("Введенное вами число является нечетным");
