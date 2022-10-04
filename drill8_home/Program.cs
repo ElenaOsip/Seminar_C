c54b8f335766c56e1ba0134868bce3ea8b1c632f
Elena <kallaur@bk.ru>
2022-10-04 03:23:30 -0400

    done

---
+++ drill8_home/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите любое число больше 1");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+if (n>1)
+
+    {while 
+        (i < n || i == n)
+        {
+            if (i%2==0)
+                Console.Write(i);
+                Console.Write(" ");
+            i++;    
+        }
+    }
+    
+else
+Console.WriteLine("перечитываем условие внимательно");
+Console.WriteLine(" ");
