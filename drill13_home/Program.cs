58e6f486776d1a6d7675a221541d045f9af2e82f
Elena <kallaur@bk.ru>
2022-10-05 11:29:09 -0400

    intToString

---
+++ drill13_home/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите любое целое число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<0)
+
+{Console.WriteLine("Я пока не знаю что делать с отрицательными числами, так что, До свидания!");
+
+}
+
+else
+{
+
+string array = n.ToString();
+
+int index = array.Length;
+
+if (index>=3)
+
+{Console.WriteLine("Третья цифра данного числа = " + array[2]);
+}
+
+else 
+{Console.WriteLine("А вот нет в вашем числе третьей цифры. Извиняйте.)");
+}
+}
