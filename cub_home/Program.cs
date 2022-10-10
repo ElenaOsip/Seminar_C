55a59139c83052b30f376820fc3d9ff16ce4192c
Elena <kallaur@bk.ru>
2022-10-10 16:42:43 -0400

    Cubing

---
+++ cub_home/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int cub = 0;
+
+for (int i = 1; i < N; i ++)
+
+{cub=Convert.ToInt32(Math.Pow(i,3));
+ Console.Write (cub + ", ");
+ }
+
+
+cub=Convert.ToInt32(Math.Pow(N,3));
+Console.WriteLine (cub);
