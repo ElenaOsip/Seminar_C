aadccdf00e3131f15db8fc19bbccbe6f4bc02903
Elena <kallaur@bk.ru>
2022-10-12 12:29:12 -0400

    arrayRandom

---
+++ a29_home/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите длину массива, который вы хотите получить. Вернее, массив какой длины вам НУЖЕН!");
+
+int length = Convert.ToInt32(Console.ReadLine());
+
+
+void newArray (int []array)
+{   int index=0;
+    int length = array.Length;
+    while (index<length)
+    {array[index] = new Random().Next(1,100);
+    index ++;}
+}
+
+void PrintArray (int[]array)
+
+{ 
+    {  int index=0;
+        int length = array.Length;
+        while (index<length)
+    {Console.Write(array[index]+ " ");
+    index ++;}
+    }
+    
+Console.WriteLine();
+}
+
+int []arr= new int[length];
+
+newArray(arr);
+PrintArray(arr);
