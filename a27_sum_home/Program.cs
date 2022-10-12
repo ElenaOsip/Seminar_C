e9e4fe038b95835b0c732d6c8c12f9d0ef2890c2
Elena <kallaur@bk.ru>
2022-10-11 21:40:07 -0400

    summa zifr

---
+++ a27_sum_home/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое положительное число");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+
+
+while (N>0)
+
+    {sum= sum+N%10;
+    N=N/10;
+    }
+
+Console.WriteLine("Сумма цифр вашего числа = " +sum);
