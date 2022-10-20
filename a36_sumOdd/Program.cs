6791a12363cc983a3d8738c9a236d74149ea4e41
Elena <kallaur@bk.ru>
2022-10-20 07:39:19 -0400

    sum odd position

---
+++ a36_sumOdd/Program.cs
@@ -0,0 +1,32 @@
+﻿void NewArray (int []array, int from, int to)
+{   int index=0;
+    int length = array.Length;
+    while (index<length)
+    {array[index] = new Random().Next(from, to);
+    index ++;}
+}
+
+void PrintArray (int[]array)
+
+{ 
+    {  int index=0;
+        int length = array.Length;
+        while (index<length)
+    {Console.Write(array[index]+ " ");
+    index ++;}
+    }
+    
+Console.WriteLine();
+}
+
+int [] arr = new int [10];
+
+NewArray (arr, -9, 10);
+PrintArray (arr);
+
+int sumOdd = arr[1];
+for (int i=3; i<arr.Length; i=i+2)
+
+    {sumOdd=sumOdd+arr[i];
+    }
+Console.WriteLine("Сумма чисел на нечетных позициях = "+ sumOdd);
