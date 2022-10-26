022d73bfc7f57cd8f1e70d54aa3bb789e5d015e0
Elena <kallaur@bk.ru>
2022-10-25 21:20:34 -0400

    finding positive numbers

---
+++ a41_home/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: 
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("введите количество элементов, которые будем проверять на позитив");
+int count = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [count];
+int pos=0;
+int i=0;
+
+while (i<=count-1)
+{ Console.WriteLine($"Введите {i+1}-й элемент");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0)
+        pos = pos +1;
+
+    i++; 
+}
+Console.WriteLine("положительных чисел введенных Вами = " + pos);
