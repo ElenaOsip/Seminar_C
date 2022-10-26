78971b191ea2a70312a5c7fe74b692a15e81e6a1
Elena <kallaur@bk.ru>
2022-10-25 21:22:43 -0400

    finding the intersection point of straight lines

---
+++ a43_home/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 43: 
+//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите два значения для первой прямой. Начнем с первого значения");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение для первой прямой");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите два значения для второй прямой. Также начнем с первого значения");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение для второй прямой");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x;
+double y;
+
+x = (b2-b1)/(k1-k2);
+y=x*k2+b2;
+
+Console.WriteLine("Прямые пересекаются в точке А с координатами ("+ x+", "+y+")");
