8329d62909218821c65a4bbc8e235d7f655547db
Elena <kallaur@bk.ru>
2022-10-26 22:33:54 -0400

    Decimal to Binar

---
+++ s42_decToBin/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 42
+//перевести число из десятичной системы исчисления в двоичную 
+
+Console.WriteLine("Введите десятичное число, которое мечтаете увидеть в двоичной системе");
+
+int dec = Convert.ToInt32(Console.ReadLine());
+string count=null;  //когда строка, то задаем не 0
+while (dec != 0)
+{
+    count = dec % 2+count; 
+    dec = dec/2;   // dec /=2;
+}
+Console.WriteLine(count);
+
+
+// while (dec != 0)
+// {
+//     count += dec % 2; 
+//     dec = dec/2;   // dec /=2;
+// }
+
+// for (int i=count.Length-1; i>=0; i--)
+// {
+//     Console.Write(count[i]);
+// }
+// Console.WriteLine();
